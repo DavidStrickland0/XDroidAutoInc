be139a29fe16add3536d624ddc9874bcc770ec4a
DavidStrickland0 <davidstrickland0@hotmail.com>
2015-09-26 07:44:35 -0400

    Initial Application Upload

---
+++ XDroidAutoInc/AndroidManifestManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace https://github.com/DavidStrickland0/XDroidAutoInc.git
+{
+    class AndroidManifestManager
+    {
+        public static void IncrementVersion(string filePath)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+            var versionNode = doc.SelectSingleNode("manifest");
+            XmlNode androidversionCode = versionNode.Attributes["android:versionCode"];
+            XmlNode androidversionName = versionNode.Attributes["android:versionName"];
+            
+            var androidversionNameSplit = androidversionName.Value.Split('.');
+            string truncatedVersionName;
+             
+            if(androidversionNameSplit.Count<string>() >1)
+                truncatedVersionName =   string.Join(".",androidversionNameSplit.Take(androidversionNameSplit.Length - 1));
+            else
+                truncatedVersionName = androidversionName.Value;
+
+            int androidVersionNumber=0;
+            int.TryParse(androidversionCode.Value,out androidVersionNumber);
+
+            androidVersionNumber++;
+            androidversionCode.Value = androidVersionNumber.ToString();
+            androidversionName.Value = string.Concat(truncatedVersionName, ".", androidVersionNumber.ToString());
+            doc.Save(filePath);
+            
+        }
+    }
+}
---
+++ XDroidAutoInc/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XDroidAutoInc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Count<string>()!=1)
+            {
+                DisplayHelp();
+            }
+            string filepath = args[0];
+            filepath = filepath.Trim('"');
+            if(!System.IO.File.Exists(filepath))
+            {
+                DisplayHelp();
+            }
+            try
+            {
+                var manifestReader = new XmlTextReader(filepath);
+                manifestReader.Read();
+
+                manifestReader.Close();
+            }
+            catch
+            {
+                DisplayHelp();
+            }
+            AndroidManifestManager.IncrementVersion(filepath);
+        }
+
+        private static void DisplayHelp()
+        {
+            System.Console.WriteLine("You must specify the full path to the Android Manifest.");
+            System.Console.WriteLine("Example: XDroidAutoInc \"C:\\SourceCode\\MyProject\\Droid\\Properties\\AndroidManifest.xml");
+            Environment.Exit(0);
+        }
+    }
+}
